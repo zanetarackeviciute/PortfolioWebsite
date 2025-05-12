463fad7de98b6f23899e86301b91f8a544f57f60
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-05-12 13:18:56 +0300

    Add animation to "Projects" section

--- website/scripts.js
+++ website/scripts.js
@@ -36,3 +36,41 @@ gsap.to(".skill", {
     stagger: 0.5,
     ease: "back.out(1.7)"
 })
+
+document.querySelectorAll('.project').forEach(project => {
+    ScrollTrigger.create({
+        trigger: project,
+        start: "top 80%",
+        once: true,
+        onEnter: () => {
+            // for letters animation: letter by letter
+            project.querySelectorAll('[data-animate="type"]').forEach(elem => {
+                const text = elem.textContent;
+                let index = 0;
+                elem.textContent = '';
+                const interval = setInterval(() => {
+                    elem.textContent += text[index];
+                    index++;
+                    if (index === text.length) clearInterval(interval);
+                }, 30);
+            })
+
+            // fro span and a appearance
+            gsap.to(project.querySelectorAll('[data-animate="fade"]'), {
+                opacity: 1,
+                y: 0,
+                duration: 0.6,
+                stagger: 0.2,
+                ease: "power2.out"
+            })
+
+            // for a whole project block
+            gsap.to(project, {
+                opacity: 1,
+                clearProps: "transform", // if I leave y: 0 - project:hover doesn't work
+                duration: 0.8,
+                ease: "power2.out"
+            })
+        }
+    })
+})
